933e0b9e15bcd1ff4fb0f769e11b4127bb69be63
ch0wm3in <32718507+ch0wm3in@users.noreply.github.com>
2020-06-26 22:28:59 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -34,12 +34,19 @@ cd $build_dir/bin
 #[ $QUIET ] && arg_q='-q' || arg_q=''
 #[ $QUIET ] && arg_s='-s' || arg_s=''
 #[ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
-lsd_release=$(curl -s https://github.com/Peltoche/lsd/releases/latest | cut -d "\"" -f 2 | cut -d "/" -f 8)
-lsd_url="https://github.com/Peltoche/lsd/releases/download/$lsd_release/lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz"
 if [ -x "$(command -v wget)" ]; then
+  lsd_release=$(wget -S -O- --max-redirect=0 --save-header https://github.com/Peltoche/lsd/releases/latest 2>&1 | grep -e "location: " | cut -d " " -f 4 | cut -d "/" -f 8)
+  lsd_url="https://github.com/Peltoche/lsd/releases/download/$lsd_release/lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz"
+  bat_release=$(wget -S -O- --max-redirect=0 --save-header https://github.com/sharkdp/bat/releases/latest 2>&1 | grep -e "location: " | cut -d " " -f 4 | cut -d "/" -f 8)
+  bat_url="https://github.com/Peltoche/lsd/releases/download/$bat_release/bat-$bat_release-x86_64-unknown-linux-gnu.tar.gz
+  
   wget $lsd_url
+  wget $bat_url
+  
   tar xf  lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz lsd-$lsd_release-x86_64-unknown-linux-gnu/lsd --strip-components=1
+  tar xf  bat-$bat_release-x86_64-unknown-linux-gnu.tar.gz bat-$bat-x86_64-unknown-linux-gnu/bat --strip-components=1
   rm lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz
+  rm bat-$bat_release-x86_64-unknown-linux-gnu.tar.gz
 fi
 #elif [ -x "$(command -v curl)" ]; then
 #  curl $arg_s -L $portable_url -o $tarname
