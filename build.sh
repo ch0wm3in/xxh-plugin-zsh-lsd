dbd9e2721856376d39ef6f87b8bd562190301b4a
ch0wm3in <32718507+ch0wm3in@users.noreply.github.com>
2020-06-27 07:51:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,11 +48,22 @@ if [ -x "$(command -v wget)" ]; then
   rm lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz
   rm bat-$bat_release-x86_64-unknown-linux-gnu.tar.gz
 fi
-#elif [ -x "$(command -v curl)" ]; then
-#  curl $arg_s -L $portable_url -o $tarname
-#else
-#  echo Install wget or curl
-#fi
+elif [ -x "$(command -v curl)" ]; then
+  lsd_release=$(curl -s https://github.com/Peltoche/lsd/releases/latest -D- | grep "location: " | cut -d "/" -f 8 | tr -d "\r")
+  bat_release=$(curl -s https://github.com/sharkdp/bat/releases/latest -D- | grep "location: " | cut -d "/" -f 8 | tr -d "\r")
+  lsd_url="https://github.com/Peltoche/lsd/releases/download/$lsd_release/lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz"
+  bat_url="https://github.com/sharkdp/bat/releases/download/$bat_release/bat-$bat_release-x86_64-unknown-linux-gnu.tar.gz"
+  
+  curl $lsd_url -OJ
+  curl $bat_url -OJ
+  
+  tar xf  lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz lsd-$lsd_release-x86_64-unknown-linux-gnu/lsd --strip-components=1
+  tar xf  bat-$bat_release-x86_64-unknown-linux-gnu.tar.gz bat-$bat_release-x86_64-unknown-linux-gnu/bat --strip-components=1
+  rm lsd-$lsd_release-x86_64-unknown-linux-gnu.tar.gz
+  rm bat-$bat_release-x86_64-unknown-linux-gnu.tar.gz
+else
+  echo Install wget or curl
+fi
 #
 #tar -xzf $tarname
 #rm $tarname
